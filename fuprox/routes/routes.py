1d585629cb94d4de6f6dd860082e43df3348a444
simons22d <denniskiruku@gmail.com>
2020-12-27 20:56:20 +0300

    d

--- fuprox/routes/routes.py
+++ fuprox/routes/routes.py
@@ -670,26 +670,27 @@ def get_book():
     user_id = request.json["user_id"]
     user = Customer.query.get(2)
     booking = Booking.query.get(129)
+    print(user)
+    print(booking)
     if user and booking:
-        if user.id == booking.user:
+
             # return the ticket
-            data = Booking.query.get(booking_id)
-            final = booking_schema.dump(data)
-            if final:
-                name = ServiceOffered.query.filter_by(name=final["service_name"]).first()
-                data = service_offer_schema.dump(name)
-                res = {
-                    "active": final["active"],
-                    "branch_id": final["branch_id"],
-                    "booking_id": final["id"],
-                    "service_name": final["service_name"],
-                    "serviced": final["serviced"],
-                    "user_id": final["user"],
-                    "start": final["start"],
-                    "code": data["code"] + final["ticket"]
-                }
-            else:
-                res = {"msg": "user/booking mismatch"}
+        data = Booking.query.get(booking_id)
+        final = booking_schema.dump(data)
+        if final:
+            name = ServiceOffered.query.filter_by(name=final["service_name"]).first()
+            data = service_offer_schema.dump(name)
+            res = {
+                "active": final["active"],
+                "branch_id": final["branch_id"],
+                "booking_id": final["id"],
+                "service_name": final["service_name"],
+                "serviced": final["serviced"],
+                "user_id": final["user"],
+                "start": final["start"],
+                "code": data["code"] + final["ticket"]
+            }
+
         else:
             res = {"msg": "user/booking mismatch"}
     else:
